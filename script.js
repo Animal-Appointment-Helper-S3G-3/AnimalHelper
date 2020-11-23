5af60d995814245e7e2da45d2461ab8c5e018ab5
arthur <lscapitalism123@gmail.com>
2020-11-23 14:49:04 +0000

    added checkSymptoms() function which checks symptoms and returns possible diagnoses. the array currently has 3 options.

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let i = 0;
-console.log(i)
+// good website for seeing symptoms and diagnoses: https://pets.webmd.com/dogs/symptoms 
+let symptomsAndDiagnoses = [["vomiting", "itch", "bad breath"],["Bacterial infection of the gastrointestinal tract", "allergies or dry skin", "Brush/clean their teeth. If it is more serious, it could be an oral infection or gum disease."]]
+function checkSymptoms(){
+  displayDiagnoses.innerHTML = "";
+  let s = symptomTextbox.value.toLowerCase();
+  for(let i = 0; i < symptomsAndDiagnoses[0].length; i++){
+    if(s.includes(symptomsAndDiagnoses[0][i])){
+      displayDiagnoses.innerHTML = symptomsAndDiagnoses[1][i]
+      break;
+    }
+  }
+}
+
+let button = document.querySelector("#submitSymptomsButton").addEventListener("click", checkSymptoms)
